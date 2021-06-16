ebffefbc2ce978e4774ce8a4fc4abfa19eaaac20
Elena Danchenko <danchenkohelen@gmail.com>
2021-06-16 10:15:36 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectElena
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Elena");
+			Console.WriteLine(Sum(2, 3));
+			Console.ReadLine();
+		}
+
+		public static int Sum(int value1, int value2) 
+		{
+			return value1 + value2;
+		}
+	}
+}
